904a58aabe379e0cb01c47cbf751ddf53c14da19
somshubham <somshubhams@gmail.com>
2016-07-31 19:53:22 +0530

    CHANGED

--- app/src/main/java/com/movie/som/popularmovie/MainActivity.java
+++ app/src/main/java/com/movie/som/popularmovie/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -37,9 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+
+            Log.v("my","my clicked");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
